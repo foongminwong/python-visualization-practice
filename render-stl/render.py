8f9e01c27c63795b45aad3eb638f131a7df2bb20
Foong Min Wong <foongmin@hotmail.com>
2019-02-18 16:19:04 -0600

    Render .stl files using matplotlib and numpy-stl

---
+++ render-stl/render.py
@@ -0,0 +1,20 @@
+# Source: https://w.wol.ph/2015/07/10/rendering-stl-files-matplotlib-numpy-stl/
+
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+# Create a new plot
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
+
+# Load the STL files and add the vectors to the plot
+your_mesh = mesh.Mesh.from_file('anormflirt.stl')
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+
+# Auto scale to the mesh size
+scale = your_mesh.points.flatten(-1)
+axes.auto_scale_xyz(scale, scale, scale)
+
+# Show the plot to the screen
+pyplot.show()
